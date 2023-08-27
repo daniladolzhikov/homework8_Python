c81c2a92e1ce1988c6d782e1a193099de421ea95
daniladolzhikov <d.dolzhikov17@gmail.com>
2023-08-27 15:25:37 +0200

    added contact search function

--- loggers.py
+++ loggers.py
@@ -1,3 +1,6 @@
+import os
+
+
 def add_contact():
     last_name = input("Введите фамилию: ")
     first_name = input("Введите имя: ")
@@ -9,3 +12,21 @@ def add_contact():
     contact_filename = f"{last_name}_{first_name}_{middle_name}.txt"
     with open(contact_filename, 'w') as file:
         file.write(contact_data)
+
+
+def search_contact():
+    search_term = input("Введите фамилию, имя или отчество для поиска: ")
+
+    found_contacts = []
+    for filename in os.listdir():
+        if filename.endswith(".txt"):
+            with open(filename, 'r') as file:
+                if search_term.lower() in file.read().lower():
+                    found_contacts.append(filename)
+
+    if found_contacts:
+        print("Найденные контакты:")
+        for contact in found_contacts:
+            print(contact)
+    else:
+        print("Контакты не найдены.")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from loggers import add_contact
+from loggers import add_contact, search_contact
 
 def main():
     while True:
