6ac5f558995302241df500f0d4772074b055284d
daniladolzhikov <d.dolzhikov17@gmail.com>
2023-08-27 16:39:58 +0200

    contact correction function added

--- loggers.py
+++ loggers.py
@@ -30,3 +30,37 @@ def search_contact():
             print(contact)
     else:
         print("Контакты не найдены.")
+
+
+import os
+
+def correction_contact(contact_filename):
+    try:
+        with open(contact_filename, 'r') as file:
+            contact_data = file.read()
+            print("Текущая информация о контакте:\n", contact_data)
+
+        last_name, first_name, middle_name = contact_filename[:-4].split('_')
+        new_last_name = input("Введите новую фамилию: ")
+        new_first_name = input("Введите новое имя: ")
+        new_middle_name = input("Введите новое отчество: ")
+        new_phone_number = input("Введите новый номер телефона: ")
+
+        new_contact_data = (
+            f"Фамилия: {new_last_name}\n"
+            f"Имя: {new_first_name}\n"
+            f"Отчество: {new_middle_name}\n"
+            f"Номер телефона: {new_phone_number}"
+        )
+
+        new_contact_filename = f"{new_last_name}_{new_first_name}_{new_middle_name}.txt"
+
+        if contact_filename != new_contact_filename:
+            os.rename(contact_filename, new_contact_filename)
+
+        with open(new_contact_filename, 'w') as file:
+            file.write(new_contact_data)
+            print("Информация о контакте обновлена.")
+    except FileNotFoundError:
+        print("Контакт не найден.")
+
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from loggers import add_contact, search_contact
+from loggers import add_contact, search_contact, correction_contact
 
 def main():
     while True:
         print("\nМеню:")
         print("1. Добавить контакт")
         print("2. Поиск контакта")
-        print("3. Выйти")
+        print("3. Изменить данные контакта")
+        print("4. Выйти")
         choice = input("Выберите действие: ")
 
         if choice == '1':
@@ -13,6 +14,9 @@ def main():
         elif choice == '2':
             search_contact()
         elif choice == '3':
+            contact_filename = input("Введите имя файла контакта для изменения: ")
+            correction_contact(contact_filename)
+        elif choice == '4':
             break
         else:
             print("Некорректный выбор. Попробуйте снова.")
