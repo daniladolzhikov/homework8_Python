2c4ac34cf05cd5ec6cbe11f4d73ce286e830e4b1
daniladolzhikov <d.dolzhikov17@gmail.com>
2023-08-27 11:16:36 +0200

    added guide interface

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+def main():
+    while True:
+        print("\nМеню:")
+        print("1. Добавить контакт")
+        print("2. Поиск контакта")
+        print("3. Выйти")
+        choice = input("Выберите действие: ")
+
+        if choice == '1':
+            add_contact()
+        elif choice == '2':
+            search_contact()
+        elif choice == '3':
+            break
+        else:
+            print("Некорректный выбор. Попробуйте снова.")
+
+if __name__ == "__main__":
+    main()
